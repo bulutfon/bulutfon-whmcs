178d27c1722c62ba549556626054196807f1e48b
Hakan ERSU <hakanersu@gmail.com>
2015-09-12 11:42:36 +0300

    Update init.php

--- modules/addons/bulutfon/init.php
+++ modules/addons/bulutfon/init.php
@@ -1,6 +1,8 @@
 <?php
-/*error_reporting(E_ALL);
-ini_set("display_errors", 1);*/
+/*
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+*/
 
 include __DIR__.'/../../../configuration.php';
 include __DIR__.'/vendor/autoload.php';
@@ -8,11 +10,3 @@ include __DIR__.'/vendor/autoload.php';
 ORM::configure('mysql:host=localhost;dbname='.$db_name);
 ORM::configure('username',$db_username);
 ORM::configure('password',$db_password);
-
-/**
- * Some helper functions to debugging.
- * @param $var
- */
-function pp($var){
-    echo "<pre>",print_r($var),"</pre>";
-}
