f7bf1240ac673b1062060d8bbd838bddc028ae11
GizemAldemir <aldemirgizem@gmail.com>
2015-10-12 15:04:55 +0300

    InvoicePaymentReminder_Reminder  eklendi!

---
+++ modules/addons/bulutfon/hooks/InvoicePaymentReminder_Reminder.php
@@ -0,0 +1,10 @@
+<?php
+
+if(!isset($InvoicePaymentReminder_Reminder)) {
+    $InvoicePaymentReminder_Reminder= function($args) use($provider,$token,$repository){
+
+    };
+}
+
+
+return $InvoicePaymentReminder_Reminder;
