063a142bef48fa56eb3de481f41f51f479ca54cf
Hakan ERSU <hakanersu@gmail.com>
2015-11-13 16:01:06 +0200

    Prevent credit messages.

--- modules/addons/bulutfon/hooks/InvoicePaid.php
+++ modules/addons/bulutfon/hooks/InvoicePaid.php
@@ -3,7 +3,7 @@ if (!isset($InvoicePaid)) {
     $InvoicePaid = function ($args) use ($provider, $token, $repository, $sms) {
         $user = $repository->findUserByInvoiceId($args['invoiceid']);
         $gsm = $repository->getFirstGsm($user);
-        if ($gsm) {
+        if ($gsm && $user->total!=0) {
             $message = $repository->getSmsMessage('InvoicePaid', [$user->firstname, $user->lastname, $user->duedate,$user->total]);
             $sms($gsm, $message);
         }
