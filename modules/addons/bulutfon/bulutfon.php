714b2b463c2e9753289446821742385063c69d41
Hakan ERSU <hakanersu@gmail.com>
2017-11-07 15:29:12 +0300

    Version update

--- modules/addons/bulutfon/bulutfon.php
+++ modules/addons/bulutfon/bulutfon.php
@@ -2,8 +2,6 @@
 include __DIR__ . '/../../../configuration.php';
 include __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ ."/core.php";
-use Bulutfon\Libraries\Helper;
-use Bulutfon\Libraries\Repository;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 if (!defined("WHMCS")) {
@@ -12,40 +10,40 @@ if (!defined("WHMCS")) {
 
 function bulutfon_config()
 {
-    $configarray = array(
+    $configarray = [
         "name" => "Bulutfon WHMCS Addon",
         "description" => "Bulutfon WHMCS Addon",
-        "version" => "1.0.3",
+        "version" => "1.1.0",
         "author" => "Hakan ERSU",
         "language" => "turkish",
-        "fields" => array(
-            "token" => array(
+        "fields" => [
+            "token" => [
                 "FriendlyName" => "Token",
                 "token" => "Uygulama Anahtarı",
                 "Type" => "text",
                 "Size" => "60",
                 "Description" => "Bulutfon API uygulama anahtarı.",
                 "Default" => ""
-            ),
-            'env' => array(
+            ],
+            'env' => [
                 'FriendlyName' => 'Urun Ortami',
                 'Type' => 'dropdown',
-                'Options' => array(
+                'Options' => [
                     'production' => 'Uretim Ortami',
                     'dev' => 'Gelistirme Ortami'
-                ),
+                ],
                 'Description' => 'Gelistirme ortami secerek gonderilecek sms\'leri gonderilmeden gorebilirisiniz.',
-            ),
-            "smstitle" => array(
+            ],
+            "smstitle" => [
                 "FriendlyName" => "SMS Basligi",
                 "smstitle" => "Sms Basligi",
                 "Type" => "text",
                 "Size" => "36",
                 "Description" => "Bulutfon Uzerinden gonderilecek sms'ler icin baslik. Bu basligin onaylanmis olmasi gerekmektedir.",
                 "Default" => ""
-            ),
-        ),
-    );
+            ],
+        ],
+    ];
 
     return $configarray;
 }
@@ -69,7 +67,7 @@ function bulutfon_activate()
     Capsule::schema()->create('mod_bulutfon_smstemplates', function ($table) {
         $table->increments('id');
         $table->string('name', 64);
-        $table->enum('type', array('client', 'admin'));
+        $table->enum('type', ['client', 'admin']);
         $table->string('admingsm', 255);
         $table->string('template', 240);
         $table->string('variables', 500);
@@ -88,7 +86,7 @@ function bulutfon_activate()
 
     Capsule::unprepared(file_get_contents(__DIR__ . "/install/templates.sql"));
 
-    return array('status' => 'success', 'description' => 'Bulutfon succesfully activated :)');
+    return ['status' => 'success', 'description' => 'Bulutfon succesfully activated :)'];
 }
 
 function bulutfon_deactivate()
@@ -96,7 +94,8 @@ function bulutfon_deactivate()
     Capsule::schema()->dropIfExists('mod_bulutfon_smstemplates');
     Capsule::schema()->dropIfExists('mod_bulutfon_messagelog');
     Capsule::schema()->dropIfExists('mod_bulutfon_usersettings');
-    return array('status' => 'success', 'description' => 'Bulutfon succesfully deactivated :(');
+
+    return ['status' => 'success', 'description' => 'Bulutfon succesfully deactivated :('];
 }
 
 function bulutfon_output($vars)
@@ -107,34 +106,33 @@ function bulutfon_output($vars)
 function bulutfon_clientarea($vars)
 {
     $hooks = Capsule::table('mod_bulutfon_smstemplates')->get();
-    $settings = Capsule::table('mod_bulutfon_usersettings')->where('clientid',$_SESSION['uid'])->first();
+    $settings = Capsule::table('mod_bulutfon_usersettings')->where('clientid', $_SESSION['uid'])->first();
     $postArray = [];
 
     if ($_POST) {
-        foreach($hooks as $hook) {
+        foreach ($hooks as $hook) {
             $postArray[$hook->name] = isset($_POST[$hook->name]) ? 1 : 0;
         }
         $postArray['all'] = isset($_POST['all']) ? 1 : 0;
         $postArray = json_encode($postArray);
 
-        Capsule::statement('INSERT INTO mod_bulutfon_usersettings (clientid,setting) VALUES(?,?) ON DUPLICATE KEY UPDATE setting = ?',[
+        Capsule::statement('INSERT INTO mod_bulutfon_usersettings (clientid,setting) VALUES(?,?) ON DUPLICATE KEY UPDATE setting = ?', [
             $_SESSION['uid'],
             $postArray,
             $postArray
         ]);
 
-
         header('Location:index.php?m=bulutfon');
     }
 
-    return array(
+    return [
         'pagetitle' => 'SMS Bildirim Ayarlari',
-        'breadcrumb' => array('index.php?m=bulutfon'=>'SMS Ayarlari'),
+        'breadcrumb' => ['index.php?m=bulutfon' => 'SMS Ayarlari'],
         'templatefile' => 'templates/clientarea/index',
         'requirelogin' => true,
-        'vars' => array(
+        'vars' => [
             'hooks' => $hooks,
-            'settings'=> (array)json_decode($settings->setting)
-        ),
-    );
+            'settings' => (array)json_decode($settings->setting)
+        ],
+    ];
 }
